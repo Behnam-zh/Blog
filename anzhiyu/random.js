950cadce7072bc42ee578a03fd72f021e6c7397a
Zehao Zhang <zehao.zh@hotmail.com>
2024-07-21 11:18:44 +0800

    Site updated: 2024-07-21 11:18:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/20/hello-world/"];function toRandomPost(){
+var posts=["2024/07/20/hello-world/","2024/07/21/解决github访问速度慢的问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
